e29e7af37a61478734876e6dfc0b8cadcb66ae60
Franz <santfra@protonmail.com>
2022-10-16 13:41:19 +0200

    Heder costruito

---
+++ src/Components/LightDarkButton.js
@@ -0,0 +1,22 @@
+import { ActionIcon, useMantineColorScheme } from "@mantine/core";
+import { IconSun, IconMoonStars } from '@tabler/icons';
+
+function LightDarkButton () {
+
+    const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+    const dark = colorScheme === 'dark';
+
+    return(
+        <div className="">
+            <ActionIcon
+                variant="outline"
+                color={dark ? "yellow" : "blue"}
+                onClick={() => toggleColorScheme()}
+                title="Toggle color scheme"
+            >
+                {dark ?  <IconSun size={18} /> : <IconMoonStars size={18} />}
+            </ActionIcon>
+        </div>
+    )
+}
+export default LightDarkButton;
--- src/Layout/MainLayout.js
+++ src/Layout/MainLayout.js
@@ -1,53 +1,128 @@
-import { ActionIcon, Burger, Container, Group } from "@mantine/core";
-import { useDisclosure } from "@mantine/hooks";
+import { Burger, ColorSchemeProvider, Container, createStyles, Group, Header, MantineProvider, Image } from "@mantine/core";
 import { useState } from "react";
+import { Link, NavLink, Outlet } from "react-router-dom";
+import LightDarkButton from "../Components/LightDarkButton";
 
-interface HeaderMiddleProps {
-    links: { link: string; label: string }[];
-}
+//css config style page 
+const useStyles = createStyles((theme) => ({
+    inner: {
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      height: 56,
+  
+      [theme.fn.smallerThan('sm')]: {
+        justifyContent: 'flex-start',
+      },
+    },
+  
+    links: {
+      width: 260,
+  
+      [theme.fn.smallerThan('sm')]: {
+        display: 'none',
+      },
+    },
+  
+    social: {
+      width: 260,
+  
+      [theme.fn.smallerThan('sm')]: {
+        width: 'auto',
+        marginLeft: 'auto',
+      },
+    },
+  
+    burger: {
+      marginRight: theme.spacing.md,
+  
+      [theme.fn.largerThan('sm')]: {
+        display: 'none',
+      },
+    },
+  
+    link: {
+      display: 'block',
+      lineHeight: 1,
+      padding: '8px 12px',
+      borderRadius: theme.radius.sm,
+      textDecoration: 'none',
+      color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.colors.gray[7],
+      fontSize: theme.fontSizes.sm,
+      fontWeight: 500,
+  
+      '&:hover': {
+        backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+      },
+    },
+  
+    linkActive: {
+      '&, &:hover': {
+        backgroundColor: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).background,
+        color: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).color,
+      },
+    },
+  }));
 
-export default function HeaderMiddle ({ links }: HeaderMiddleProps) {
+function MainLayout () {
 
-    const [opened, {toggle}] = useDisclosure(false);
-    const [active, setActive] = useState(links[0].link);
+    //--- lista dei link 
+    const links = [
+        {link:"/", label:"Home"},
+        {link:"/app", label:"App"},
+    ]
+
     const { classes, cx } = useStyles();
 
+    const [colorScheme, setColorScheme] = useState ('light');
+    const toggleColorScheme = (value) =>
+        setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+
+    const [opened, setOpened] = useState(false);
+    const title = opened ? 'Close navigation' : 'Open navigation';
+    const [active, setActive] = useState(links[0].link);
+
+    //--- Crea gli ite m del menu
     const items = links.map((link) => (
-        <a
+        <Link
             key={link.label}
-            href={link.link}
-            className={cx(classes.link, { [classes.linkActive]: active === link.link})}
-            onClick={(event) => {
-                event.preventDefault();
+            to={link.link}
+            className={cx(classes.link, { [classes.linkActive]: active === link.link })}
+            onClick={() => {
                 setActive(link.link);
             }}
         >
             {link.label}
-        </a>
-    ));
+        </Link>
+      ));
 
     return(
-        <Heder height={56} mb={120}>
-            <Container className={classes.inner}>
-                <Burger opened={opened} onClick={toggle} size="sm" className={classes.burger} />
-                <Group className={classes.links} spacing={5}>
-                    {items}
-                </Group>
+        <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme} >
+            <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS >
+                <Header>
+                    <Container className={classes.inner}>
+                        {/* <Burger opened={opened} onClick={toggle} size="sm" className={classes.burger} /> */}
+                        <Burger
+                            opened={opened}
+                            onClick={() => setOpened((o) => !o)}
+                            title={title}
+                            size="sm"
+                            className={classes.burger}
+                        />
+                        <Group className={classes.links} spacing={5}>
+                            {items}
+                        </Group>
+                        
+                        {/* Logo  */}
+                        
+                        <LightDarkButton/>
 
-                {/* logo */}
-
-                <Group spacing={0} className={classes.social} position="right" noWrap>
-                    <ActionIcon size="lg">
-                        <IconBrandYoutube size={18} stroke={1.5} />
-                    </ActionIcon>
-                    <ActionIcon size="lg">
-                        <IconBrandYoutube size={18} stroke={1.5} />
-                    </ActionIcon>
-                    <ActionIcon size="lg">
-                        <IconBrandYoutube size={18} stroke={1.5} />
-                    </ActionIcon>
-                </Group>
-            </Container>
-        </Heder>
+                    </Container>
+                </Header>
+                <Outlet/>
+            </MantineProvider>
+        </ColorSchemeProvider>
     )
 }
+
+export default MainLayout;
